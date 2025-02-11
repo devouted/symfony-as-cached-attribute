60c71c5ffbe7e1d60091dc0e1b96734d99e7fda5
Marcin Sadowski <marcin.sadowski@contractors.roche.com>
2025-02-11 09:35:10 +0100

    updated resolving of cache key parameters

--- src/Attribute/AsCachedResponse.php
+++ src/Attribute/AsCachedResponse.php
@@ -7,12 +7,20 @@ use Attribute;
 #[Attribute(Attribute::TARGET_METHOD)]
 class AsCachedResponse
 {
+    /**
+     * Attribute defining caching behavior for a controller response.
+     *
+     * @param int         $ttl             Time-to-live for cache (in seconds, default: 3600)
+     * @param string|null $etag            ETag value for the response (optional)
+     * @param string|null $expires         Expiration date for the cache (e.g., 'tomorrow')
+     * @param bool        $isPublic        Whether the cache is public (shared) or private, public will tell browser to get reponse from local cache (default: public)
+     * @param array       $cacheKeyParams  List of request parameters to include in the cache key generation
+     */
     public function __construct(
         public int     $ttl = 3600,
         public ?string $etag = null,
         public ?string $expires = null,
-        public bool    $isPublic = true
-    )
-    {
-    }
+        public bool    $isPublic = true,
+        public array   $cacheKeyParams = []
+    ) {}
 }
--- src/Listener/CachedResponseListener.php
+++ src/Listener/CachedResponseListener.php
@@ -14,31 +14,39 @@ use Symfony\Contracts\Cache\CacheInterface;
 
 class CachedResponseListener
 {
-    public function __construct(private CacheInterface $cache)
-    {
-    }
+    const CACHE_HEADER_STATE_NAME = 'X-Cache';
+    const CACHE_HEADER_MISS = 'Miss';
+    const CACHE_HEADER_HIT = 'Hit';
+
+    public function __construct(private CacheInterface $cache) {}
 
     public function onKernelControllerArguments(ControllerArgumentsEvent $event): void
     {
         $controller = $event->getController();
 
-        if (is_string($controller) || is_object($controller)) {
-            $reflectionMethod = new ReflectionMethod($controller, "__invoke");
+        if (is_string($controller) || is_object($controller) || is_array($controller)) {
+
+            $reflectionMethod = new ReflectionMethod(...$this->resolveCallableForReflection($controller));
+
             $attributes = $reflectionMethod->getAttributes(AsCachedResponse::class);
             if (empty($attributes)) {
                 return;
             }
             /** @var AsCachedResponse $cacheAttribute */
             $cacheAttribute = $attributes[0]->newInstance();
+
             $event->getRequest()->attributes->set('_cache', $cacheAttribute);
 
-            $request = $event->getRequest();
-            $cacheKey = $this->generateCacheKey($request, $event);
+            $request  = $event->getRequest();
+            $cacheKey = $this->generateCacheKey($request, $event, $cacheAttribute->cacheKeyParams);
+
             $request->attributes->set('_cache_key', $cacheKey);
 
             $cachedResponse = $this->cache->get($cacheKey, fn() => null);
             if ($cachedResponse instanceof Response) {
-                //Jeśli znaleziono w cache, zwracamy gotową odpowiedź i pomijamy kontroler
+
+                $cachedResponse->headers->set(self::CACHE_HEADER_STATE_NAME, self::CACHE_HEADER_HIT);
+                //if cache is present, return cached response and bypass controller action execution
                 $event->setController(fn() => $cachedResponse);
             }
         }
@@ -49,7 +57,6 @@ class CachedResponseListener
         $request = $event->getRequest();
 
         $cacheAttribute = $request->attributes->get('_cache');
-
         if (!$cacheAttribute instanceof AsCachedResponse) {
             return;
         }
@@ -61,9 +68,9 @@ class CachedResponseListener
         }
 
         $response->setCache([
-            'max_age' => $cacheAttribute->ttl,
+            'max_age'  => $cacheAttribute->ttl,
             's_maxage' => $cacheAttribute->ttl,
-            'public' => $cacheAttribute->isPublic,
+            'public'   => $cacheAttribute->isPublic,
         ]);
 
         if ($cacheAttribute->etag) {
@@ -76,33 +83,48 @@ class CachedResponseListener
 
         $cacheKey = $request->attributes->get('_cache_key');
 
+        $response->headers->set(self::CACHE_HEADER_STATE_NAME, self::CACHE_HEADER_MISS);
+
         $this->cache->get($cacheKey, function ($item) use ($response, $cacheAttribute) {
             $item->expiresAfter($cacheAttribute->ttl);
+
             return $response;
         });
 
         $event->setResponse($response);
     }
 
-    private function generateCacheKey(Request $request, ControllerArgumentsEvent $event): string
+    private function generateCacheKey(Request $request, ControllerArgumentsEvent $event, array $cacheKeyParams): string
     {
-        $cachedParams = [];
+        $cacheKeyParams   = [];
+        $methodReflection = $this->resolveCallableForReflection($event->getController());
         foreach ($event->getNamedArguments() as $paramName => $arg) {
+            //if param is type of DTO where we can chose parameters that need to be used for cache key
             if (is_object($arg)) {
                 $reflectionClass = new \ReflectionClass($arg);
                 foreach ($reflectionClass->getProperties() as $property) {
                     $attributes = $property->getAttributes(AsCachedRequestParameter::class);
                     if (!empty($attributes)) {
-                        $cachedParams[$property->getName()] = $property->getValue($arg);
+                        $cacheKeyParams[$property->getName()] = $property->getValue($arg);
                     }
                 }
-            } else {
-                $attributes = (new \ReflectionParameter([$event->getController(), "__invoke"], $paramName))->getAttributes(AsCachedRequestParameter::class);
-                if (!empty($attributes)) {
-                    $cachedParams[$paramName] = $arg;
-                }
+            }
+            //the param it self might have a AsCachedRequestParameter attribute
+            $attributes = (new \ReflectionParameter($methodReflection, $paramName))->getAttributes(AsCachedRequestParameter::class);
+            if (!empty($attributes)) {
+                $cacheKeyParams[$paramName] = $arg;
             }
         }
-        return 'cached_response_' . md5($request->getPathInfo() . serialize($cachedParams));
+
+        return 'cached_response_' . md5($request->getPathInfo() . serialize($cacheKeyParams));
+    }
+
+    private function resolveCallableForReflection(array|string|object $controller): array
+    {
+        if (is_array($controller)) {
+            return [$controller[0], $controller[1]];
+        }
+
+        return [$controller, "__invoke"];
     }
 }
